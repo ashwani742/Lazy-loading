f9fa5fd70f1833296e23ef5c70694ad7db13522e
Ashwani kumar <ashwanikumar07042000@gmail.com>
2025-06-12 14:30:54 +0530

    done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Admin } from './admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+//    { path:'admin',component:Admin}
+{ path:'admin',loadComponent:()=>import('./admin/admin').then((c)=>c.Admin)}
+];
